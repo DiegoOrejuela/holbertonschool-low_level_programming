92c296e0fffc8cb8d8e42a3212eedbfb75e19f5c
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-02-08 01:08:48 +0000

    9-print_comb-correction update file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = 48;
+
+	while (num <= 57)
+	{
+		putchar(num);
+		if (num != 57)
+		{
+		putchar(44);
+		putchar(32);
+		}
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
