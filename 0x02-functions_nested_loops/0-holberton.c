224d4d1ace177c8cef3742d67e1d8a1aa4dff44d
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-02-12 20:44:16 +0000

    0-holberton.c-correction last character in array

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -9,10 +9,10 @@ int main(void)
 	int i;
 	char ch[] = "Holberton";
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i < 9; i++)
 	{
 		_putchar(ch[i]);
 	}
-	_putchar(10);
+	_putchar(0x0A);
 	return (0);
 }
