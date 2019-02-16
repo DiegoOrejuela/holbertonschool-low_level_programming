35d4f8d4e49ddeae8abafda8b378a472312cce6a
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-02-16 22:38:11 +0000

    4-print_most_numbers.c-code init

---
+++ 0x03-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ *print_most_numbers - print the natural numbers whitout '2' and '4'.
+ *@void: nothing.
+ *
+ *Description: The function iterate inside conditional in a for loop each number
+ *natural and print them with the function _putchar.
+ *Return: nothing.
+ */
+void print_most_numbers(void)
+{
+	int dg;
+
+	for (dg = '0'; dg <= '9'; dg++)
+	{
+		if (dg != '2' && dg != '4')
+		{
+			_putchar(dg);
+		}
+	}
+	_putchar('\n');
+}
