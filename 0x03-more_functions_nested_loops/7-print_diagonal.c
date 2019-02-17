9deaf6fa4e64f1165cee82cf5e75c31418da851a
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-02-17 05:04:10 +0000

    7-print_diagonal.c-code init

---
+++ 0x03-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+/**
+ *print_diagonal - print '_' n times.
+ *@n: times that '/' print.
+ *
+ *Description: The function print '/' n times.
+ *Return: nothing.
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+	int k = 1;
+
+	if (n == 0 || n < 0)
+	{
+		_putchar('\n');
+	}
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			if (n == 1)
+			{
+				_putchar(92);
+				_putchar('\n');
+			}
+			if (n != 1)
+			{
+				for (j = 0; j < k; j++)
+				{
+					_putchar(' ');
+				}
+				_putchar(92);
+				_putchar('\n');
+				if (k != n)
+				{
+					k++;
+				}
+			}
+		}
+	}
+}
