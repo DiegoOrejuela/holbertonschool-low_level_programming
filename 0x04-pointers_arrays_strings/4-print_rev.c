6a0457aec4541f6085a1fad22e655c7c25c732fe
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-02-19 22:26:11 +0000

    4-print_rev.c-correction iteration print

--- 0x04-pointers_arrays_strings/4-print_rev.c
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
  *print_rev - prints a string, in reverse.
  *@s: string to avaluate.
@@ -15,6 +16,7 @@ void print_rev(char *s)
 	{
 		i++;
 	}
+	i--;
 	while (i >= 0)
 	{
 		_putchar(*(s + i));
