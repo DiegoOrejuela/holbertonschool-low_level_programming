0a0d814b0319b1f8ca933d9f2962abd95276d25a
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-02-20 22:44:53 +0000

    8-print_array.c-code init

---
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *print_array - prints n elements of an array of integers, followed
+ *by a new line.
+ *@a: array to avaluate.
+ *@n: number of elements for print.
+ *
+ *Description: The function take an array and iterate printing the values
+ *selected.
+ **/
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i != (n - 1))
+		{
+			printf("%d, ", a[i]);
+		}
+		else
+		{
+			printf("%d\n", a[i]);
+		}
+	}
+
+}
