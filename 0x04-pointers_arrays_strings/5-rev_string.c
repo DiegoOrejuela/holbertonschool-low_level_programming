096970a99b33c7a50d27a0aef5938104e541d7d7
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-02-20 01:20:51 +0000

    5-rev_string.c-code init

---
+++ 0x04-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ *rev_string - function that reverses a string.
+ *@s: string to avaluate.
+ *
+ *Description: The function take a string and iterate it with first a loop
+ *while until the value '\0' for to save the long array. After, again
+ *iterate 's' for to assign the new values.
+ **/
+void rev_string(char *s)
+{
+	int i = 0;
+	int j = 0;
+	char aux;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+
+	i--;
+	while (i > 4)
+	{
+		aux = *(s + i);
+		*(s + i) = *(s + j);
+		*(s + j) = aux;
+		i--;
+		j++;
+	}
+}
