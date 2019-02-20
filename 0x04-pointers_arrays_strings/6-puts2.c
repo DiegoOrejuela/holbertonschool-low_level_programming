5f3ac4930facc009fb33171e10d66da5dc03d894
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-02-20 15:16:25 +0000

    6-puts2-correction condition if

--- 0x04-pointers_arrays_strings/6-puts2.c
+++ 0x04-pointers_arrays_strings/6-puts2.c
@@ -10,10 +10,13 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	while (*(str + i))
+	while (str[i])
 	{
-		_putchar(*(str + i));
-		i += 2;
+		if (str[i] == 0 || (str[i] % 2) == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
 	}
 	_putchar('\n');
 }
