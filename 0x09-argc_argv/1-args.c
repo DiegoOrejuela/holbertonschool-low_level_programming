cccc7e581e1b00420cc7d3ca0e03e5a3ba45bceb
DiegoOrejuela <you@example.com>
2019-03-02 22:51:13 +0000

    code init

---
+++ 0x09-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * main - check the code for Holberton School students.
+ * @argc: number arguments in argc[].
+ * @argv: array de pointers to console arguments.
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
