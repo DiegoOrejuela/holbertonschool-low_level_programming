c10fb73f3ce42bdabd4bb13073cd4105338ac2ed
DiegoOrejuela <you@example.com>
2019-03-03 03:43:43 +0000

    correction return 0 to finally

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -14,10 +14,6 @@ int main(int argc, char *argv[])
 	int j;
 	int result = 0;
 
-	if (argc == 1)
-	{
-		printf("0\n");
-	}
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
@@ -28,9 +24,6 @@ int main(int argc, char *argv[])
 				return (1);
 			}
 		}
-	}
-	for (i = 1; i < argc; i++)
-	{
 		result += atoi(argv[i]);
 	}
 	printf("%d\n", result);
