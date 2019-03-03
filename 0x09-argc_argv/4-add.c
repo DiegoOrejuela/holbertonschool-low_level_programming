58787380c2f05b443365d41a576c7fba10225790
DiegoOrejuela <you@example.com>
2019-03-03 00:21:22 +0000

    code init

---
+++ 0x09-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ * main - check the code for Holberton School students.
+ * @argc: number arguments in argc[].
+ * @argv: array de pointers to console arguments.
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int result;
+	(void)argc;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (1);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		if (atoi(argv[i]) != 0)
+		{
+			result += atoi(argv[i]);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n", result);
+	return (0);
+}
