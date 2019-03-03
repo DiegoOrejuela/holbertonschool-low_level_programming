59c542e864b4a7c7adcea5d8c40b8ed96a5f525e
DiegoOrejuela <you@example.com>
2019-03-03 01:26:48 +0000

    line 13: space ; condiion when argv is 0

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -10,8 +10,7 @@
 int main(int argc, char *argv[])
 {
 	int i;
-	int result;
-	(void)argc;
+	int result = 0;
 
 	if (argc == 1)
 	{
@@ -20,14 +19,21 @@ int main(int argc, char *argv[])
 	}
 	for (i = 1; i < argc; i++)
 	{
-		if (atoi(argv[i]) != 0)
+		if (atoi(argv[i]) == 0)
 		{
-			result += atoi(argv[i]);
+			;
 		}
 		else
 		{
-			printf("Error\n");
-			return (1);
+			if (atoi(argv[i]) != 0)
+			{
+				result += atoi(argv[i]);
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
 	}
 	printf("%d\n", result);
