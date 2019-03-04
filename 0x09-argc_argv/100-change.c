623168c340796d29674bee27a18dc2ed06f63d03
DiegoOrejuela <you@example.com>
2019-03-04 20:59:26 +0000

    code init

---
+++ 0x09-argc_argv/100-change.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *@argc : number arguments.
+ *@argv : arguments.
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int cents = 0;
+	int money;
+
+	if (argc == 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	money = atoi(argv[1]);
+	if (money < 0)
+	{
+		printf("0\n");
+	}
+	if (money / 25 != 0)
+	{
+		cents += money / 25;
+		money -= 25 * (money / 25);
+	}
+	if (money / 10 != 0)
+	{
+		cents += money / 10;
+		money -= 10 * (money / 10);
+	}
+	if (money / 5 != 0)
+	{
+		cents += money / 5;
+		money -= 5 * (money / 10);
+	}
+	if (money / 2 != 0)
+	{
+		cents += money / 2;
+		money -= 2 * (money / 2);
+	}
+	if (money / 1 != 0)
+	{
+		cents += money / 1;
+		money -= 1 * (money / 1);
+	}
+	printf("%d\n", cents);
+	return (0);
+}
