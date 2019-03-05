65f73a77042446e0be10792ee067a8f415729805
DiegoOrejuela <you@example.com>
2019-03-05 22:52:27 +0000

    code init

---
+++ 0x0A-malloc_free/2-str_concat.c
@@ -0,0 +1,62 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * str_concat - returned pointer should point to a newly allocated space
+ * in memory which contains the contents of s1, followed by the contents
+ * of s2, and null terminated.
+ * @s1: first string for assign.
+ * @s2: second string for assign.
+ *
+ * Return: Null o the pointer with the changues.
+ */
+char *str_concat(char *s1, char *s2)
+{
+
+	char *p;
+	int i;
+	int j;
+	int size_s1 = _strlen(s1);
+	int size_s2 = _strlen(s2);
+
+
+	p = malloc(size_s1 + size_s2);
+	if (!p)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < (size_s1 - 1); i++)
+	{
+		p[i] = s1[i];
+	}
+	p[i] = ' ';
+	i++;
+	for (j = 0 ; j < size_s2; j++)
+	{
+		p[i] = s2[j];
+		i++;
+	}
+	return (p);
+
+}
+
+/**
+ *_strlen - returns the length of a string.
+ *@s: string to avaluate.
+ *
+ *Description: The function take a string and iterate it with a loop while
+ *until the value '\0'. The times iteration are saved in a int variable 'i'.
+ *Return: the length of a string.
+ **/
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
