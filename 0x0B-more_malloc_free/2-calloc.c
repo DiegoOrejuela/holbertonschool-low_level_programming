75a1ebfa12fdc3958fa043ac400ab90e1d12bb5e
DiegoOrejuela <you@example.com>
2019-03-08 01:54:02 +0000

    code init

---
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ * _calloc - allocates memory using malloc..
+ * @nmemb: number member to add.
+ * @size: size type;
+ *
+ * Return: pointer changued or NULL.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *p;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	p = malloc(nmemb * size);
+	if (p == NULL)
+	{
+		exit(98);
+	}
+	for (i = 0; i < nmemb; i++)
+	{
+		p[i] = 0;
+	}
+	return (p);
+}
