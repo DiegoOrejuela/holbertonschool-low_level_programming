461c8a6f43b57488f6bdb15de44a0b9ca04537a4
DiegoOrejuela <you@example.com>
2019-03-14 02:50:18 +0000

    code init

---
+++ 0x0E-function_pointers/100-main_opcodes.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ * @argc: arguments count.
+ * @argv: array pointers to arguments.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	if (atoi(argv[1]) < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	return (0);
+
+}
