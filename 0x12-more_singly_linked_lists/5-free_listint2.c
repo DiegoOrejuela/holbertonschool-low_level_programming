aed6332b33f317d74183aae42550529e6e63844b
DiegoOrejuela <you@example.com>
2019-03-26 16:16:57 +0000

    code init

---
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list.
+ *
+ * @head: list type listint_t.
+ *
+ * Return: nothing.
+ */
+void free_listint2(listint_t **head)
+{
+	for ( ; *head != NULL; *head = (*(*head)).next)
+		free(*head);
+}
