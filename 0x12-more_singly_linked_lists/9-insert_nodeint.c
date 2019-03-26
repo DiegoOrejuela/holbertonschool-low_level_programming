e41934faa63d0615e2d797274a7b2fd32befcf0f
DiegoOrejuela <you@example.com>
2019-03-26 23:38:49 +0000

    feature: add option index 0

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,6 +21,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	counter = 0;
 	for (iterator = *head; iterator != NULL; iterator = (*iterator).next)
 	{
+		if (idx == 0)
+			return (add_nodeint(head, n));
 		if (counter == idx - 1)
 			break;
 		counter++;
@@ -39,4 +41,34 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	(*iterator).next = new;
 	return (new);
+}
+
+/**
+ * add_nodeint - adds a new node at the beginning of a listint_t list.
+ * @head: list type listint_t.
+ * @n: value n for the new node.
+ *
+ * Return: the new node or NULL.
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *newnode;
+
+	newnode = malloc(sizeof(listint_t));
+	if (newnode == NULL)
+	{
+		free(newnode);
+		return (NULL);
+	}
+	(*newnode).n = n;
+
+	if (*head == NULL)
+	{
+		(*newnode).next = NULL;
+		*head = newnode;
+		return (newnode);
+	}
+	(*newnode).next = *head;
+	*head = newnode;
+	return (newnode);
 }
