4376337811b7ea54c970172e24f790dd6bb376dc
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-03-28 22:04:33 +0000

    files init

---
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include "holberton.h"
+
+int _pow_recursion(int x, int y);
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int.
+ * @b: pointing to a string of 0 and 1 chars.
+ *
+ * Return: the converted number, or 0 if.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i, j;
+	unsigned int result;
+
+	if (b == NULL)
+		return (0);
+
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+	}
+
+	result = 0;
+	j = 0;
+	i--;
+	for ( ; i >= 0; i--, j++)
+	{
+		printf("valor b = %d\n", b[i] - '0');
+		result += (b[i] - '0') * _pow_recursion(2, j);
+	}
+
+	return (result);
+}
+
+/**
+ *_pow_recursion - returns the value of x raised to the power of y.
+ *@x: number raised.
+ *@y: number power.
+ *
+ *Return: the result pow. If the number is less that 0, return -1.
+ **/
+int _pow_recursion(int x, int y)
+{
+	int rs = 0;
+
+	if (y == 0)
+	{
+		rs = 1;
+	}
+	if (y == 1)
+	{
+		rs = x;
+	}
+	if (y < 0)
+	{
+		rs = -1;
+	}
+	if (y > 1)
+	{
+		rs = x * _pow_recursion(x, y - 1);
+	}
+	return (rs);
+}
