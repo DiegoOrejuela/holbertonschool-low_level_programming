4cc80d7a1a18cedd027e5c4315a22332299c6efe
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-03-29 03:48:07 +0000

    code init

---
+++ 0x13-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * clear_bit - sets the value of a bit to 0 at a given index.
+ * @n: number to convert.
+ * @index: postion the number to set.
+ *
+ * Return: 1 if it worked, or -1 if an error occurred.
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (!(index > (sizeof(n) * 8)))
+	{
+		*n = ~(~*n | 1 << index);
+		return (1);
+	}
+	return (-1);
+}
