4100921d09f397af900a6a2aa4bd258df8635222
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-04-02 21:39:26 +0000

    code init

---
+++ 0x14-file_io/0-read_textfile.c
@@ -0,0 +1,53 @@
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * read_textfile - check the code for Holberton School students.
+ * @filename: path of the file.
+ * @letters: characters to print.
+ *
+ * Return: lenght of the characters readed or 0 if is error.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, rs, length;
+	char *buf;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		return (0);
+	}
+
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+	{
+		return (0);
+	}
+
+	length = 0;
+	length = read(fd, buf, letters);
+	if (length == -1)
+	{
+		close(fd);
+		free(buf);
+		return (0);
+	}
+
+	rs = write(1, buf, letters);
+	if (rs == -1)
+	{
+		close(fd);
+		free(buf);
+		return (0);
+	}
+	close(fd);
+	free(buf);
+
+	return (length);
+}
