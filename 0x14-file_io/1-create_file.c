783dde1a66ad65ad76668f396572edddee5f19a4
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-04-03 00:43:29 +0000

    code init

---
+++ 0x14-file_io/1-create_file.c
@@ -0,0 +1,59 @@
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+int _strlen(char *s);
+
+/**
+ * create_file - creates a file.
+ * @filename: path of the file.
+ * @text_content: characters to print.
+ *
+ * Return: 1 on success, -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd = 0, rs = 0;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+		text_content = "";
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 00600);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+
+	rs = write(fd, text_content, _strlen(text_content));
+	if (rs == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+
+	close(fd);
+	return (1);
+}
+
+#include "holberton.h"
+/**
+ *_strlen - returns the length of a string.
+ *@s: string to avaluate.
+ *
+ *Description: The function take a string and iterate it with a loop while
+ *until the value '\0'. The times iteration are saved in a int variable 'i'.
+ *Return: the length of a string.
+ **/
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
