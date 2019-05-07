f3a7639cab76d8c7109b8c8afc5da8342aef2248
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-05-07 02:20:36 +0000

    code init

---
+++ 0x17-dynamic_libraries/unrand.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+int rand(void)
+{
+	static int i;
+
+	i++;
+
+	if (i == 1)
+		return(9);
+	else if (i == 2)
+		return(8);
+	else if (i == 3)
+		return(10);
+	else if (i == 4)
+		return(24);
+	else if (i == 5)
+		return(75);
+	else
+		return(9);
+}
