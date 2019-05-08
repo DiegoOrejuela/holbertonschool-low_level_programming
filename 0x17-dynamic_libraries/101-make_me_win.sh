6bb794d45c801891dec0bf66e2eb9abb456e951b
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-05-08 21:26:46 +0000

    file

--- 0x17-dynamic_libraries/101-make_me_win.sh
+++ 0x17-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl -Lso "unrand.so" "https://github.com/DiegoOrejuela/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/unrand.so"
-export LD_PRELOAD=unrand.so
+curl -Lso "/tmp/unrand.so" "https://github.com/DiegoOrejuela/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/unrand.so"
+export LD_PRELOAD=/tmp/unrand.so
