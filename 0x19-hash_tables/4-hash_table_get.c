5e89a6e592bf08f96c88afde92ce170a3611f6ce
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-05-31 04:01:51 +0000

    code init and add string.h in .h

---
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a value associated with a key.
+ * @ht: retrieves a value associated with a key.
+ * @key: key you are looking for.
+ *
+ * Return: value associated with the element, or NULL if key
+ * couldn’t be found.
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *temp;
+
+	if (key == NULL || key == '\0' || ht == NULL)
+		return (NULL);
+
+	index = key_index((unsigned char *)key, (*ht).size);
+
+	temp = (*ht).array[index];
+	while (temp != NULL)
+	{
+		if (strcmp((*temp).key, key) == 0)
+		{
+			return ((*temp).value);
+		}
+		temp = (*temp).next;
+	}
+	return (NULL);
+}
