81018e34ac76efc437173365df7d7ad9755f3a2a
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-06-18 21:57:48 +0000

    T2: betty file

--- 0x1B-sorting_algorithms/2-selection_sort.c
+++ 0x1B-sorting_algorithms/2-selection_sort.c
@@ -7,18 +7,18 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i=0, j, pivote;
+	size_t i = 0, j, pivote;
 	int aux;
 
 	if (array && size > 1)
 	{
-		while(i < size - 1)
+		while (i < size - 1)
 		{
 			pivote = i;
 			j = i + 1;
 			while (j < size)
 			{
-				if(array[pivote] > array[j])
+				if (array[pivote] > array[j])
 				{
 					pivote = j;
 				}
