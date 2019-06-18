6a8479a1784c1e272af1d07cf06714c0676adb92
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-06-18 21:54:57 +0000

    T2: sorts an array of integers in ascending order using the Selection sort algorithm

--- 0x1B-sorting_algorithms/2-selection_sort.c
+++ 0x1B-sorting_algorithms/2-selection_sort.c
@@ -1,11 +1,37 @@
 #include "sort.h"
 
 /**
- * selection_sort -
- *
- * Return: 
+ * selection_sort - sorting by selection
+ * @array: array
+ * @size: size
  */
 void selection_sort(int *array, size_t size)
 {
+	size_t i=0, j, pivote;
+	int aux;
 
+	if (array && size > 1)
+	{
+		while(i < size - 1)
+		{
+			pivote = i;
+			j = i + 1;
+			while (j < size)
+			{
+				if(array[pivote] > array[j])
+				{
+					pivote = j;
+				}
+				j++;
+			}
+			if (i != pivote)
+			{
+				aux = array[i];
+				array[i] = array[pivote];
+				array[pivote] = aux;
+				print_array(array, size);
+			}
+			i++;
+		}
+	}
 }
