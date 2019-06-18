ec7eeb687bbb2efbb24df80d434741b07dd7cd28
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-06-18 20:30:57 +0000

    changes add directory oficcial

---
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+/**
+ * bubble_sort - sorts an array of integers in ascending order using the
+ * Bubble sort algorithm.
+ *
+ * @array: list of elements int.
+ * @size: size of array.
+ *
+ * Return: nothing.
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t size_temp, i, j;
+	int temp;
+
+	size_temp = size;
+
+	for (j = 1; j != size_temp; )
+	{
+		for (i = 0; i < size_temp - 1; i++)
+		{
+			if (array[i + 1] < array[i])
+			{
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+				print_array(array, size);
+			}
+		}
+		size_temp--;
+	}
+
+}
---
+++ 0x1B-sorting_algorithms/1-insertion_sort_list.c
@@ -0,0 +1,59 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorting with insertion sort algorithm
+ * @list: head of the list
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *marker, *pivote;
+
+	if (*list && (*list)->next)
+	{
+		marker = (*list)->next;
+		while (marker)
+		{
+			pivote = marker->prev;
+			if (pivote)
+			{
+				if (marker->n < pivote->n)
+				{
+					swap_node(list, pivote, marker);
+					pivote = pivote->prev;
+					if (marker->prev)
+						marker = marker->prev;
+					print_list(*list);
+				}
+				else
+				{
+					;
+				}
+			}
+			if (marker)
+				marker = marker->next;
+		}
+
+	}
+}
+/**
+ *swap_node - swap nodes
+ * @head: head
+ * @node1: node1 to swap
+ * @node2: node2 to swap
+ */
+void swap_node(listint_t **head, listint_t *node1, listint_t *node2)
+{
+	listint_t *temp;
+
+	temp = node1->prev;
+	if (temp)
+		temp->next = node2;
+	else
+		*head = node2;
+	node2->prev = temp;
+	node1->prev = node2;
+	node1->next = node2->next;
+	if (node1->next)
+		node1->next->prev = node1;
+	node2->next = node1;
+}
