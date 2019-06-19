20695770ba9659d16b9dd9b6670b8ef8dd0c5576
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-06-19 21:42:41 +0000

    T3: function that sorts an array of integers in ascending order using the Quick sort algorithm

--- 0x1B-sorting_algorithms/3-quick_sort.c
+++ 0x1B-sorting_algorithms/3-quick_sort.c
@@ -1,11 +1,53 @@
 #include "sort.h"
+/**
+ * recursive_quicksort - resursion quick sorting
+ * @array: array of int.
+ * @lo: start point.
+ * @hi: end point.
+ * @size: size array.
+ *
+ * Return: nothing.
+ */
+void recursive_quicksort(int *array, int lo, int hi, size_t size)
+{
+	int j, aux;
+	int i = lo - 1;
+
+	for (j = lo; j < hi; j++)
+	{
+		if (array[j] < array[hi])
+		{
+			i++;
+			aux = array[j];
+			array[j] = array[i];
+			array[i] = aux;
+			if (i != j)
+				print_array(array, size);
+		}
+	}
+	i++;
+	if (array[hi] != array[i])
+	{
+		aux = array[hi];
+		array[hi] = array[i];
+		array[i] = aux;
+		if (i != hi)
+			print_array(array, size);
+	}
+	if (lo < i - 1)
+		recursive_quicksort(array, lo, i - 1, size);
+	if (i + 1 < hi)
+		recursive_quicksort(array, i + 1, hi, size);
+
+}
+
 
 /**
- * quick_sort -
- *
- * Return: 
+ * quick_sort - quick sorting
+ * @array: array
+ * @size: size
  */
 void quick_sort(int *array, size_t size)
 {
-
+	recursive_quicksort(array, 0, size - 1, size);
 }
