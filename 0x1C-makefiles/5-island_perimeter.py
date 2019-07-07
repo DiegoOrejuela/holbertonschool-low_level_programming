119f224703c9230301af1fe734e092410325c785
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-07-07 01:43:24 +0000

    T5: version 3.0

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -9,29 +9,34 @@ def island_perimeter(grid):
     """Create a function def island_perimeter(grid):
     that returns the perimeter of the island described in grid:
     """
-    if len(grid) == 0 or grid == [[]]:  # Case: grid is empty
-        return 0
-
-    y_1, y_2, x_1, x_2 = -1, -1, len(grid), -1
+    perimeter = 0
 
     for i in range(len(grid)):
         for j in range(len(grid[0])):
-            if grid[i][j] == 1 and y_1 == -1:  # Set 1er value y(?, *)
-                y_1 = i
-                y_2 = i
-            elif grid[i][j] == 1:  # Set 2nd value y(*, ?)
-                y_2 = i
+            if grid[i][j] == 1:
+                # print("({}, {})".format(i, j))  # Position in grid
 
-            if grid[i][j] == 1 and j < x_1:  # Set 1er value x(?, *)
-                x_1 = j
-                x_2 = j
-            elif grid[i][j] == 1 and j > x_2:  # Set 2nd value x(*, ?)
-                x_2 = j
+                if grid[i][j - 1] == 0 and j != 0:  # Case review: <- [1]
+                        perimeter += 1
+                elif j == 0:
+                    perimeter += 1
 
-    if y_1 == -1:  # case: 1(land zone) was not found
-        return 0
-#   print("y({}, {}) - x({}, {})".format(y_1, y_2, x_1, x_2)) Coordenates
-    height = len(range(y_1, y_2 + 1))  # Calc: y_1 .. y_2 there is n distance.
-    length = len(range(x_1, x_2 + 1))  # Calc: x_1 .. x_2 there is n distance.
+                try:
+                    if grid[i][j + 1] == 0:  # Case review: [1] ->
+                        perimeter += 1
+                except:
+                    perimeter += 1
 
-    return (height * 2) + (length * 2)  # Calc: perimeter land zone
+                if grid[i - 1][j] == 0 and i != 0:  # Case review: ^[1]
+                        perimeter += 1
+                elif i == 0:
+                    perimeter += 1
+
+                try:
+                    if grid[i + 1][j] == 0:  # Case review: [1]v
+                        perimeter += 1
+                except:
+                    perimeter += 1
+                # print(" --- Perimeter {}".format(perimeter))
+
+    return perimeter
